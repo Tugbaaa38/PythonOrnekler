3c5dbdc344d7a204897cee583bb10e99ed1b702b
Tugba <77529943+Tugbaaa38@users.noreply.github.com>
2021-05-14 17:01:06 +0300

    Add files via upload

---
+++ pozitifBolenSayisiBulma.py
@@ -0,0 +1,10 @@
+#ekrandan alinan sayinin kac tane pozitif boleni oldugunu bulan program
+
+bolenSayisi=0
+sayi=int(input("Sayi giriniz:"))
+
+for i in range(1,sayi+1):
+    if sayi%i==0:
+        bolenSayisi+=1
+
+print(f"{sayi} sayisinin {bolenSayisi} tane pozitif boleni vardir.")
